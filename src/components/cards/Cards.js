e421aa5ec4354ed6379f411eea07b7a6b570a60a
Milchmaa <marc.welz03@gmail.com>
2023-02-03 20:02:35 +0100

    some minor changes

---
+++ src/components/cards/Cards.js
@@ -0,0 +1,14 @@
+import './Cards.scss';
+
+function Cards() {
+  return (
+    <div className="cards">
+        <div className='cards_header'>
+            <h3>About me</h3>
+        </div>
+        <div className='cards_body'></div>
+    </div>
+  );
+}
+
+export default Cards;
--- src/pages/app/App.js
+++ src/pages/app/App.js
@@ -1,9 +1,11 @@
+import Cards from '../../components/cards/Cards';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello World</h1>
+      <Cards></Cards>
     </div>
   );
 }
